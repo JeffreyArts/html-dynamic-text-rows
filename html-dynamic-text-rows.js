f573e80d7aef33f0a0b2b0957d2fadf57fbcd281
JeffreyArts <sjeffff@gmail.com>
2020-12-14 00:35:07 +0100

    Bugfixes

--- html-dynamic-text-rows.js
+++ html-dynamic-text-rows.js
@@ -8,10 +8,15 @@ var htmlDynamicTextRowsComponentHelper = {
         domElement.initialized = true;
 
         var rows = parseInt(domElement.getAttribute("dynamic-text-rows"), 10);
+        var originHeight = null
+        if (domElement.style["height"]) {
+            originHeight = domElement.style["height"];
+        }
 
         domElement.style["line-height"] = 1;
         domElement.style["display"] = "inline-block";
         domElement.style["padding"] = 0;
+        domElement.style["height"] = "auto";
         domElement.style["word-break"] = "break-word";
 
         var height = 1;
@@ -25,22 +30,26 @@ var htmlDynamicTextRowsComponentHelper = {
                 max = Math.floor(domElement.clientHeight/height) <= rows;
             }
 
-            console.log(max, Math.floor(domElement.clientHeight/height), rows);
             height++;
             if (height > 1000) {
                 // safety feature
                 max = false;
             }
         }
-        
+
         if (rows == 1) {
             domElement.style["font-size"] = height - 2 + "px";
         }
 
         domElement.style["line-height"] = null;
+        domElement.style["height"] = null;
         domElement.style["display"] = null;
         domElement.style["padding"] = null;
         domElement.style["word-break"] = null;
+
+        if (originHeight != null) {
+            domElement.style["height"] = originHeight;
+        }
     },
     documentResize: function documentResize() {
         if (htmlDynamicTextRowsResizeTimeout) {
